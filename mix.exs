49c7538065c1b508d2b2eb37bd0c3c7572d07204
🦀 adia-dev <abdoudu78130@gmail.com>
2024-02-29 22:21:48 +0100

    🚨 Installed the Credo dep
    Provides linting and diagnostic advice

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule Leitner.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:credo, "~> 1.6", only: [:test, :dev], runtime: false}
     ]
   end
 
